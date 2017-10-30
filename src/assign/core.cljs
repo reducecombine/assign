2ca08cc412f43722aeb347cc014936ef5358c01e
vemv <vemv@users.noreply.github.com>
2017-10-30 09:08:51 +0100

    Initial implementation

--- src/assign/core.cljs
+++ src/assign/core.cljs
@@ -1,17 +1,38 @@
 (ns assign.core
   (:require
    [om.core :as om]
-   [sablono.core :refer-macros [html]]))
+   [sablono.core :refer-macros [html]]
+   [clojure.string :as str])
+  (:import
+   [goog.testing PseudoRandom]))
 
-(enable-console-print!)
+(defonce app-state
+  (atom {:assignee nil}))
 
-(defonce app-state (atom {}))
+(def reviewers
+  [nil ;; have to include nil, since the last number of a random number will never be 0
+   :jborell
+   :aramos
+   :vemv
+   :mrivero
+   :jsanchez])
+
+(defn assign! [text-input]
+  (when-let [n (re-find #"[0-9]+" text-input)]
+    (let [random-numbers (-> n (js/parseInt 10) PseudoRandom. .random str (str/split ".") last reverse)
+          random-numbers (map int random-numbers)
+          chosen (first (map (partial get reviewers)
+                             random-numbers))]
+      (swap! app-state assoc :assignee chosen))))
 
 (om/root (fn [data owner]
            (reify om/IRender
              (render [_]
                (html
-                [:div]))))
+                [:div
+                 [:input {:on-change #(-> % .-target .-value assign!)}]
+                 "Assignee is: "
+                 (-> data :assignee str)]))))
          app-state
          {:target (js/document.getElementById "app")})
 
